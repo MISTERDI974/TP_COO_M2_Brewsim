47637ab3d94ffdb758410d09cbf351f73fa87ba6
Vincent Dinnat <dnv3097a@u1-105-17d.univ-tlse3.fr>
2023-09-27 17:32:14 +0200

    methode str et cost

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -6,20 +6,23 @@ class Departement(models.Model):
     numero = models.IntegerField()
     prix_m2 = models.IntegerField()
 
-    # def __str__(self):
-    #    return "numero : " self.numero
+    def __str__(self):
+        return "departement : " f"{self.numero}"
 
 
 class Machine(models.Model):
     nom = models.CharField(max_length=100)
     prix = models.IntegerField()
 
-    # def __str__(self):
-    # return "nom : " self.nom
+    def __str__(self):
+        return self.nom
 
 
 class Ingredient(models.Model):
     nom = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nom
 
 
 class Action(models.Model):
@@ -28,16 +31,27 @@ class Action(models.Model):
     durée = models.IntegerField()
     ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return "Action : " f"{self.commande}"
+
 
 class Recette(models.Model):
     nom = models.CharField(max_length=100)
     action = models.ForeignKey(Action, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nom
 
 
 class QuantiteIngredient(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
     quantite = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.ingredient.nom}" " : "f"{self.quantite}" "Kg"
+
+    #def cost(self, departement):
+    #    quantite*prix_departement
 
 class Usine(models.Model):
     departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
@@ -46,8 +60,20 @@ class Usine(models.Model):
     recette = models.ManyToManyField(Recette)
     stock = models.ManyToManyField(QuantiteIngredient)
 
+    def __str__(self):
+        return "Usine " f"{self.departement}"
+
+    def cost(self) :
+        return taille*departement.prix_m2 + machine.prix
+
+
+
 
 class Prix(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
     departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
     prix = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.ingredient.nom}"
+        " coûte " f"{self.prix} dans le "  f"{self.departement.numero}"
